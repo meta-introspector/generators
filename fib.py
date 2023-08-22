af914fcae1ce6c695a9faaacf5fa5ae14e8b9a32
mike dupont <mike.dupont@introspector.local>
2023-08-22 13:31:14 -0400

    v1

--- fib.py
+++ fib.py
@@ -1,10 +1,17 @@
 import pprint
-limit = 11
+limit = 20
 # this program is optimized to unroll loops and use globals for speed
 # there must be a better way to do this. 
 # Your hierarchical tree structure (nested dictionaries) goes here
 tree = { # fibbonacci | prime | integers
+    0: {},
+    1: {},
 } 
+# now find the primes in a sieve, and look down to find the fib
+primes = {
+    2: 1,
+}
+
 index = { #number -> factors
 }
 amax = 0  #the max numbe we saw so far
@@ -20,7 +27,7 @@ def generate_fibonacci(limit):
         if a not in tree:
             tree[a] = {} #fib as no prime yet
         a, b = b, a + b
-        #index[b] = [a,"+",b] #add to index
+        index[b] = [a,"+",b] #add to index
         ismax(b)
 
 generate_fibonacci(limit)
@@ -36,70 +43,27 @@ for a in number_list:
             index[v] = [a,"*",b]
             ismax(v)
 
-
-# now find the primes in a sieve, and look down to find the fib
-primes = {
-}
-
-def find_fibs(v):
-    for x in fibs: #loop
-        if x < v: #the first fib under the v        
-            if v not in index: #not compound
-                tree[x][v] = [] #prime under a fib
-                primes[v]=x #parent
-                print("Prime",x,v)
-                return x
-            else: #compounds lets add them
-                if x in primes:
-                    if x in primes:
-                        fib = primes[x]
-                        #print(x,tree[fib])
-                        tree[fib][x].append(x)
-                        return x
-                    else:
-                        print("DEBUG3",x,tree[fib])
-                else:# are fibs
-                    #print("DEBUG1",x)
-                    #print(tree[fib])
-                    pass
-
-#for v in number_list:
+# sieve 
 def scan(x):
     y = x-1  #find the next prime
     if x < limit:
-    while y > 1:
-        if y in primes:
-            #pass
-            print("DEUB",y)
-        else:
-            y = y - 1 # start looking one down               
-
-            if y in primes :
-                fib = primes[y]
-                if y in tree[fib]:
-                    part = tree[fib][y]
-                    part.append(x)
-                    print("DEBUG",
-                          primes[y],
-                          tree[primes[y]][y],
-                          x)
-                else:
-                    # primes
-                    print("DEBUG2",
-                          primes[y],
-                          tree[primes[y]],
-                          x)
-                    x = find_fibs(v)
-                    primes[v]=x #parent
-
-# Now sweep up the primes and attach them to the fibs
+        while y > 0:
+            y = y - 1 # start looking one down
+            if y in tree:
+                #print("F",y)
+                return y
+    return -1
+                    
+# Now sieve/sweep up the primes and attach them to the fibs
 for x in range(amax):
     if x not in index:
-        index[x] = [x]        #now scan up to find the first
-        if x > 0:
-            scan(x)
+        if x < limit:
+            #print("P",x)
+            primes[x]=scan(x)
+            index[x] = [x]
+        # now scan up to find the first
 
 pprint.pprint({
-    "FIB":tree,
-    "primes":primes,
+    #"FIB":tree,
+#    "primes":primes,
     "index": index})
